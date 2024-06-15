e0da520ae5f64cfa0605c69439806865ba92957e
0x0netech <stephens.druej@gmail.com>
2024-06-15 22:40:34 +0900

    font color

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -11,7 +11,7 @@ const ExpCard: React.FC<ExpCardProps> = ({ earn, color, profit, flag }) => {
   return (
     <div className="grid grid-col-2 w-1/3  grid-col-1 px-1">
       <div className="group w-full rounded-lg bg-[#272A30] p-2 transition relative duration-300 cursor-pointer hover:translate-y-[3px] hover:shadow-[0 -8px 0px 0px #2196f3]">
-        <p className={`text-sm md:text-lg text-[#F39E09]`}>{earn}</p>
+        <p className="text-sm md:text-lg text-[#F39E09]">{earn}</p>
         <div className="flex flex-row justify-center align-middle pt-2">
           <p className="text-white text-2xl">
             {flag ? (
