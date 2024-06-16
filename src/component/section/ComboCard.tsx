fe9191a96c4ad5352e3fad529adf496f2aac210e
0x0netech <stephens.druej@gmail.com>
2024-06-16 15:00:21 +0900

    card

--- src/component/section/ComboCard.tsx
+++ src/component/section/ComboCard.tsx
@@ -5,7 +5,7 @@ interface ComboCardProps {
 const ComboCard: React.FC<ComboCardProps> = ({ image, content }) => {
   return (
     <div className="group w-full rounded-lg bg-gradient-to-b from-pink-50 to-indigo-600 pt-[1px] pr-[1px] pl-[1px]">
-      <div className="group w-full rounded-lg bg-[#272A30] p-2">
+      <div className="group w-full rounded-lg transition relative duration-300 cursor-pointer hover:translate-y-[3px] hover:shadow-[0 -8px 0px 0px #2196f3] bg-[#272A30] p-2 h-[150px]">
         <div className="flex flex-row justify-center align-middle pt-2">
           <img src={image} alt="iamge" className="w-[70px] h-[70px]" />
         </div>
