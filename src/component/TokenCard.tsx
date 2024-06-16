e3856f615590b7f51dfb61d2479f1be299cefb27
0x0netech <stephens.druej@gmail.com>
2024-06-16 19:12:49 +0900

    fix

--- src/component/TokenCard.tsx
+++ src/component/TokenCard.tsx
@@ -2,16 +2,22 @@ interface TokenCardProps {
   image: string;
   content: string;
   value: string;
+  revune: string;
 }
-const TokenCard: React.FC<TokenCardProps> = ({ image, content, value }) => {
+const TokenCard: React.FC<TokenCardProps> = ({
+  image,
+  content,
+  value,
+  revune,
+}) => {
   return (
-    <div className="group w-full rounded-lg transition relative duration-300 cursor-pointer hover:translate-y-[3px] hover:shadow-[0 -8px 0px 0px #2196f3] bg-[#272A30] h-[150px]">
+    <div className="w-full rounded-lg transition relative duration-300 cursor-pointer hover:translate-y-[3px] hover:shadow-[0 -8px 0px 0px #2196f3] bg-[#272A30] h-[150px]">
       <div className="flex grid-cols-2 p-2">
         <div className="mx-auto my-auto min-w-16">
           <img src={image} alt="iamge" className="w-[60px] h-[60px]" />
         </div>
         <div className="w-full pl-2 sm:pl-4">
-          <p className="text-left text-white">Fan tokens</p>
+          <p className="text-left text-white">{content}</p>
           <p className="text-left text-gray-400 pt-2 text-sm">
             Profile per hour
           </p>
@@ -25,7 +31,22 @@ const TokenCard: React.FC<TokenCardProps> = ({ image, content, value }) => {
           </div>
         </div>
       </div>
-      <p className="text-sm md:text-lg text-white pt-4 ">{content}</p>
+      <div className="flex h-[3px] bg-slate-700 shadow-md rounded-md mx-auto my-auto">
+        <div className="flex h-[2px] bg-slate-800 shadow-xl w-full" />
+      </div>
+      <div className="flex flex-row mt-2 divide-x divide-gray-700">
+        <div className="w-1/4">
+          <p className="text-white items-center">|v|</p>
+        </div>
+        <div className="flex">
+          <img
+            src="image/dollar.png"
+            alt="iamge"
+            className="w-[20px] h-[20px] mt-[2px] ml-4"
+          />
+          <p className="pl-2 text-white">{revune}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/component/section/Markets.tsx
+++ src/component/section/Markets.tsx
@@ -3,8 +3,18 @@ import TokenCard from "../TokenCard";
 const Markets = () => {
   return (
     <div className="flex grid-cols-2 gap-4">
-      <TokenCard image="/image/fantoken.png" content="street" value="64.1k" />
-      <TokenCard image="/image/staking.png" content="street" value="10.1k" />
+      <TokenCard
+        image="/image/fantoken.png"
+        content="Fan tokens"
+        value="64.1k"
+        revune="234.23K"
+      />
+      <TokenCard
+        image="/image/staking.png"
+        content="Staking"
+        value="10.1k"
+        revune="212.23k"
+      />
     </div>
   );
 };
